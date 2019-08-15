947a58f25592d69cd9bd61e96e62b61d5c4e5ab2
Tanvir Islam Streame <streametanvir@gmail.com>
2019-08-15 17:11:12 +0600

    init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+  })
+]
+};
